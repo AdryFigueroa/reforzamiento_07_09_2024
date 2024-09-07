aa012e3d17bed7a6f47380ac05f1263cabbd3bce
AdryFigueroa <factoraudiovisual@gmail.com>
2024-09-07 13:08:54 -0400

    actualizacion

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function cambiarImagen1() { 
+    let laImagen = document.getElementById("esmeralda");
+    laImagen.src = "./assets/img/pudu-esmeralda.jpeg";
+}
+    function cambiarImagen2() { 
+    let laImagen = document.getElementById("esmeralda");
+    laImagen.src = "./assets/img/pudu2.jpeg";
+}
